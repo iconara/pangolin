63e55e27065003ed9963205542a979f6bc1fdc2a
Theo <theo@iconara.net>
2009-08-02 20:09:00 +0200

    The Jar task now outputs proper JAR archives.
    Added #add_manifest_attribute instead of the manifest accessor, and #manifest for building the manifest string.
    Added #create_zipfile which builds the ZIP output file.
    Added #archive_path which maps from file system paths to archive paths.

--- lib/jrake/jar.rb
+++ lib/jrake/jar.rb
@@ -1,14 +1,19 @@
 require 'java'
+require 'pp'
 
 
 module JRake
+
+  include_class 'java.io.FileOutputStream'
+  include_class 'java.util.zip.ZipOutputStream'
+  include_class 'java.util.zip.ZipEntry'
+  
 
   class Jar
   
     attr_accessor :output,      # path
                   :base_dir,    # path
                   :files,       # array
-                  :manifest,    # hash
                   :compression, # number (0-9)
                   :verbose      # boolean
                 
@@ -28,8 +33,60 @@ module JRake
       }
     end
     
+    def add_manifest_attribute( name, value )
+      if name =~ /^[A-Za-z0-9][A-Za-z0-9\-_]*$/
+        @manifest[name] = value
+      else
+        raise ArgumentError, "Malformed attribute name: #{name}"
+      end
+    end
+    
     def execute( io = $stderr )
       io.puts "jar ..." if @verbose
+      
+      create_zipfile
+    end
+    
+    def create_zipfile
+      zipstream = ZipOutputStream.new(FileOutputStream.new(@output))
+      
+      @files.each do |file|
+        f = File.new(file)
+        
+        while buffer = f.read(65536)
+          zipstream.put_next_entry(ZipEntry.new(archive_path(file)))
+          zipstream.write(buffer.to_java_bytes, 0, buffer.length)
+          zipstream.close_entry
+        end
+        
+        f.close
+      end
+      
+      manifest_str = manifest
+      
+      zipstream.put_next_entry(ZipEntry.new('META-INF/MANIFEST.MF'))
+      zipstream.write(manifest_str.to_java_bytes, 0, manifest_str.length)
+      zipstream.close_entry
+      
+      zipstream.close
+    end
+    
+    def archive_path( path )
+      if @base_dir
+        prefix = @base_dir + (@base_dir =~ /\/$/ ? '' : '/')
+      
+        if 0 == path.index(prefix)
+          return path.slice(prefix.length, path.length - prefix.length)
+        end
+      end
+      
+      path
+    end
+    
+    def manifest
+      @manifest.keys.inject("") do |str, key|
+        str + "\n#{key}: #{@manifest[key]}"
+      end + "\n"
     end
   
     # // These are the files to include in the ZIP file
--- spec/jar_spec.rb
+++ spec/jar_spec.rb
@@ -46,4 +46,91 @@ describe Jar do
     
   end
   
+  describe "#archive_path" do
+    
+    it "should remove the base dir path from the archive path" do
+      @jar.base_dir = 'build'
+
+      @jar.archive_path('build/com/example/HelloWorld.class').should == 'com/example/HelloWorld.class'
+    end
+    
+    it "should not remove anything from a file not in the base dir path" do
+      @jar.base_dir = 'build'
+      
+      @jar.archive_path('some/other/path').should == 'some/other/path'
+    end
+    
+    it "should not remove anything from a file path if the base dir path is nil" do
+      @jar.base_dir = nil
+      
+      @jar.archive_path('some/other/path').should == 'some/other/path'
+    end
+    
+  end
+  
+  describe "#manifest/#add_manifest_attribute" do
+    
+    it "should include the Built-By key" do
+      @jar.manifest.include?('Built-By').should be_true
+    end
+    
+    it "should include added attributes" do
+      @jar.add_manifest_attribute('abc', '123')
+      
+      @jar.manifest.include?('abc: 123').should be_true
+    end
+    
+    it "should write the attributes on the right format" do
+      @jar.add_manifest_attribute('one', '1')
+      @jar.add_manifest_attribute('two', '2')
+      @jar.add_manifest_attribute('three', '3')
+      
+      @jar.manifest.include?("one: 1\n").should be_true
+      @jar.manifest.include?("two: 2\n").should be_true
+      @jar.manifest.include?("three: 3\n").should be_true
+    end
+    
+    it "should only use the last set value of an attribute" do
+      @jar.add_manifest_attribute('one', '2')
+      @jar.add_manifest_attribute('one', '1')
+      
+      @jar.manifest.include?('one: 2').should_not be_true
+      @jar.manifest.include?('one: 1').should be_true
+    end
+    
+    it "should reject an empty attribute name" do
+      lambda {
+        @jar.add_manifest_attribute('', '432')
+      }.should raise_error(ArgumentError)
+    end
+
+    it "should reject nil as attribute name" do
+      lambda {
+        @jar.add_manifest_attribute(nil, 'Hello world')
+      }.should raise_error(ArgumentError)
+    end
+    
+    it "should reject an attribute name containing a space" do
+      lambda {
+        @jar.add_manifest_attribute('Hello World', 'foo')
+      }.should raise_error(ArgumentError)
+    end
+    
+    it "should reject an attribute name containing a colon" do
+      lambda {
+        @jar.add_manifest_attribute('Hello:World', 'foo')
+      }.should raise_error(ArgumentError)
+    end
+    
+    it "should accept a variety of legal attribute names" do
+      lambda {
+        @jar.add_manifest_attribute('Manifest-Version', 'foo')
+        @jar.add_manifest_attribute('Created-By', 'foo')
+        @jar.add_manifest_attribute('Signature-Version', 'foo')
+        @jar.add_manifest_attribute('Class-Path', 'foo')
+      }.should_not raise_error
+    end
+        
+  end
+  
 end
