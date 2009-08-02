4f7f38cc43752fd6dcc1c3bc96a29df93145f6da
Theo <theo@iconara.net>
2009-08-02 15:44:34 +0200

    Imports Javac and Jar and declares a utility method for each.

--- lib/jrake.rb
+++ lib/jrake.rb
@@ -1,1 +1,19 @@
-require 'jrake/javac'
+require File.expand_path(File.dirname(__FILE__)) + '/jrake/javac'
+require File.expand_path(File.dirname(__FILE__)) + '/jrake/jar'
+
+
+def javac( source_files )
+  javac = Javac.new(source_files)
+  
+  yield javac
+  
+  javac.execute
+end
+
+def jar( files )
+  jar = Jar.new(files)
+  
+  yield jar
+  
+  jar.execute
+end
